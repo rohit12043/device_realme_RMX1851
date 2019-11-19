9eb6abbd3fa4e7bbe8f97b009c7d4fe9a3687791
rohit12043 <36263440+rohit12043@users.noreply.github.com>
2019-11-19 10:24:06 +0530

    RMX1851: RMX1921->RMX1971

--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -11,10 +11,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := lineage_RMX1971
+PRODUCT_DEVICE := RMX1851
+PRODUCT_NAME := lineage_RMX1851
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 5 Pro
+PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -22,8 +22,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-realme
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20190909.095224 release-keys"
 
-BUILD_FINGERPRINT := "realme/RMX1971/RMX1971:9/PKQ1.190101.001/1566976638:user/release-keys"
+BUILD_FINGERPRINT := "realme/RMX1851/RMX1851:9/PKQ1.190101.001/1566976638:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="RMX1971" \
-    TARGET_DEVICE="RMX1971"
+    PRODUCT_NAME="RMX1851" \
+    TARGET_DEVICE="RMX1851"
